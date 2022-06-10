e283f7b78380d290b48c1e15b11f05ec4ac7acc3
Longs Pemun Gotar <developerlongs@gmail.com>
2022-06-10 21:33:55 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Moralis from "moralis";
+import { useMoralis, useWeb3Transfer } from "react-moralis";
+import { ConnectButton, useNotification } from "web3uikit";
 
 function App() {
+  const { isAuthenticated } = useMoralis();
+  const [amount, setAmount] = useState("");
+  const { fetch, isFetching } = useWeb3Transfer({
+    type: "native",
+    amount: Moralis.Units.ETH(Number(amount)),
+    receiver: "0xC34ad4A95adCD9021182fd5607ED822DB738E7c4",
+  });
+
+  const dispatch = useNotification();
+
+  const handleInvalidAmount = () => {
+    dispatch({
+      type: "error",
+      message: "Please, enter a valid amount to donate",
+      title: "Invalid Amount",
+
+      position: "topL",
+    });
+  };
+
+  const handleNotLogin = () => {
+    dispatch({
+      type: "error",
+      message: "Please, connect with metamask to continue",
+      title: "Login",
+
+      position: "topL",
+    });
+  };
+
+  const handleDonate = () => {
+    if (!isAuthenticated) {
+      handleNotLogin();
+    } else if (isAuthenticated && Number(amount) === 0) {
+      handleInvalidAmount();
+    } else {
+      fetch();
+    }
+
+    setAmount("");
+  };
+
+  console.log("Console is working!");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <img src="/fren.png" alt="logo" />
+        <div className="right-section">
+          <ul>
+            <li>
+              {/* <button>Connect Wallet</button> */}
+              <ConnectButton />
+            </li>
+          </ul>
+        </div>
       </header>
-    </div>
+
+      <div className="intro">
+        <h1>Giving it back to where we have all started.</h1>
+        <p>
+          Web3bridge has been a blessing for all of us. <br />
+          It's time we give back to where we have all have started.
+        </p>
+      </div>
+
+      <div className="input">
+        <input
+          type="number"
+          value={amount}
+          placeholder="Enter amount to donate"
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+        />
+
+        <button onClick={handleDonate} disabled={isFetching}>
+          Donate
+        </button>
+      </div>
+      <footer>© Handcrafted from scratch with 💖 by devlongs.</footer>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from "web3uikit";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider
+      appId="i0C9apvK4eaYPZpkMmsEsE953r0V5CpTMivqZeim"
+      serverUrl="https://yjii7wxsmnzn.usemoralis.com:2053/server"
+    >
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
+    </MoralisProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
